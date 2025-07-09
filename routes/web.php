4fba8e30fd8fc3ba5d992dfbfe399c98dc7a317a
Obasi, Mary Olanma <olanmamary2@gmail.com>
2025-07-09 12:46:36 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
+
+Route::get('/debug', function () {
+    return [
+        'env' => app()->environment(),
+        'key_set' => config('app.key') !== null,
+        'db_path' => config('database.connections.sqlite.database'),
+        'db_exists' => file_exists(config('database.connections.sqlite.database')),
+    ];
+});
 Route::get('/', function () {
     return view('welcome');
 });
+
